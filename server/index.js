0dcb7b4701b63d4d9d8f25a9d4e4f31d6f5e4bd8
Ian J <gftfrmgd9@gmail.com>
2026-06-23 10:34:04 -0400

    feat: add users CRUD to Express server
    - Add routes/users.js with GET /, GET /:id, POST / against users table
    - Mount users router at /users in index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
+// Create and setup app instance
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
 app.use(express.json());
 
+// Router setup
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
+
+// Start the server
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
