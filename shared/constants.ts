fc7d7b9c4c1b654c4754c8e21a65216dcd2dac12
Ian J <gftfrmgd9@gmail.com>
2026-07-31 18:52:45 -0400

    refactor: add shared file for character limits

---
+++ shared/constants.ts
@@ -0,0 +1,8 @@
+// Constants for max lengths of data fields
+// IMPORTANT NOTE: Changing these values will automatically apply everywhere, 
+// EXCEPT for the database schema. That must be changed manually.
+export const MAX_LENGTHS = {
+    skillLabel: 60,
+    treeTitle: 80,
+    statusLabel: 30,
+} as const;
