39504c2d6da5be66c795c24b385858536f0c11a9
Ian J <gftfrmgd9@gmail.com>
2026-08-31 22:34:02 -0400

    feat: move tree detail edit button next to name

--- client/src/pages/TreePage.tsx
+++ client/src/pages/TreePage.tsx
@@ -161,10 +161,12 @@ function TreePage() {
         <div className="tree-page">
             <header className="tree-page-header">
                 <div className="tree-page-meta">
-                    <div className="tree-page-title-row">
-                        {/* If viewing this as a non-owner, show owner's display name */}
-                        <span className="eyebrow">{!isOwner && (tree.owner_display_name + "'s")} Skill Tree</span>
+                    {/* If viewing this as a non-owner, show owner's display name */}
+                    <span className="eyebrow">{!isOwner && (tree.owner_display_name + "'s")} Skill Tree</span>
 
+                    <div className="tree-page-title-row">
+                        <h1 className="tree-page-title">{treeName}</h1>
+                        
                         {/* Tree details edit popup */}
                         {isOwner && (
                             <PopupButton 
@@ -210,7 +212,6 @@ function TreePage() {
                         )}
                     </div>
 
-                    <h1 className="tree-page-title">{treeName}</h1>
                     {treeDescription && <p className="tree-page-description">{treeDescription}</p>}
                 </div>
 
